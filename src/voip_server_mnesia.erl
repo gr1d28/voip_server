42c88e0df947c10a352710c1937e79cb66d4780d
gr1d28 <egorka.belayv.04@mail.ru>
2026-05-31 10:28:17 +0000

    Save voip_server_mnesia.erl

--- src/voip_server_mnesia.erl
+++ src/voip_server_mnesia.erl
@@ -4,139 +4,173 @@
 -include_lib("voip_server/include/voip_server_db.hrl").
 
 -export([ensure_replication/0, setup_cluster/0]).
--export([wait_for_sync/1]).
+-export([wait_for_sync/1, create_master_tables/0]).
 
-%% @doc Обеспечить репликацию данных на все узлы
-%% Вызывается при старте на ЛЮБОМ узле (и master, и slave)
--spec ensure_replication() -> ok | {error, term()}.
-ensure_replication() ->
-    io:format("Ensuring Mnesia replication on ~p~n", [node()]),
-
-    %% Mnesia уже запущена nksip
-    case mnesia:system_info(is_running) of
-        yes -> ok;
-        no -> {error, mnesia_not_running}
-    end,
-
-    %% Получаем все узлы кластера
-    AllNodes = ?NODE_LIST,
-    CurrentNodes = mnesia:system_info(db_nodes),
-
-    io:format("All nodes: ~p, Current schema nodes: ~p~n", [AllNodes, CurrentNodes]),
-
-    %% 1. Расширяем схему на все узлы
-    MissingNodes = AllNodes -- CurrentNodes,
-    if MissingNodes =/= [] ->
-        io:format("Adding missing nodes to schema: ~p~n", [MissingNodes]),
-        add_nodes_to_schema(MissingNodes);
-    true ->
-        io:format("All nodes already in schema~n")
-    end,
-
-    %% 2. Убеждаемся, что все таблицы созданы и реплицированы
-    ensure_tables_replicated(AllNodes),
-
-    %% 3. Ждем синхронизации
+%% @doc Установка кластера с нуля
+-spec setup_cluster() -> ok.
+setup_cluster() ->
+    io:format("Setting up Mnesia cluster~n"),
+    
+    %% Останавливаем Mnesia на всех узлах
+    lists:foreach(fun(Node) ->
+        case rpc:call(Node, mnesia, system_info, [is_running]) of
+            yes -> 
+                io:format("Stopping Mnesia on ~p~n", [Node]),
+                rpc:call(Node, mnesia, stop, []);
+            _ -> 
+                ok
+        end
+    end, ?NODE_LIST),
+    
+    timer:sleep(1000),
+    
+    %% Удаляем старую схему
+    lists:foreach(fun(Node) ->
+        io:format("Deleting schema on ~p~n", [Node]),
+        rpc:call(Node, mnesia, delete_schema, [[Node]])
+    end, ?NODE_LIST),
+    
+    timer:sleep(2000),
+    
+    %% Создаем схему на всех узлах
+    io:format("Creating schema on all nodes~n"),
+    rpc:call(?MASTER_NODE, mnesia, create_schema, [?NODE_LIST]),
+    
+    timer:sleep(2000),
+    
+    %% Запускаем Mnesia на всех узлах
+    lists:foreach(fun(Node) ->
+        io:format("Starting Mnesia on ~p~n", [Node]),
+        rpc:call(Node, mnesia, start, [])
+    end, ?NODE_LIST),
+    
+    timer:sleep(2000),
+    
+    %% Создаём таблицы на master узле
+    io:format("Creating tables on master~n"),
+    create_master_tables(),
+    
+    timer:sleep(2000),
+    
+    %% Добавляем копии таблиц на slave узлы
+    add_table_copies_to_slaves(),
+    
+    timer:sleep(2000),
+    
+    %% Ждём синхронизации
     wait_for_sync(30000),
-
-    %% 4. Проверяем статус
+    
+    %% Проверяем статус
     print_replication_status(),
-
+    
+    io:format("Cluster setup complete~n"),
     ok.
 
-%% @private Добавление узлов в схему
-add_nodes_to_schema(Nodes) ->
-    %% Сначала расширяем конфигурацию
-    case mnesia:change_config(extra_db_nodes, Nodes) of
-        {ok, _} -> ok;
-        {error, Reason1} ->
-            io:format("Warning: change_config error: ~p~n", [Reason1])
-    end,
-
-    %% Добавляем каждый узел в схему
-    lists:foreach(fun(Node) ->
-        case mnesia:add_table_copy(schema, Node, disc_copies) of
-            {atomic, ok} ->
-                io:format("Schema added to ~p~n", [Node]);
-            {aborted, {already_exists, _, _}} ->
-                ok;
-            {aborted, Reason2} ->
-                io:format("Failed to add schema to ~p: ~p~n", [Node, Reason2])
-        end
-    end, Nodes).
-
-%% @private Обеспечить репликацию таблиц
-ensure_tables_replicated(AllNodes) ->
+%% @doc Создание таблиц на master узле
+create_master_tables() ->
     Tables = ?TABLES_NAME_LIST,
-
+    
     lists:foreach(fun(Table) ->
-        %% Проверяем существует ли таблица
-        case mnesia:table_info(Table, exists) of
-            true ->
-                %% Таблица существует, проверяем копии
-                CurrentCopies = get_table_copies(Table),
-                MissingNodes = AllNodes -- CurrentCopies,
-                if MissingNodes =/= [] ->
-                    io:format("Adding table ~p copies to: ~p~n", [Table, MissingNodes]),
-                    lists:foreach(fun(Node) ->
-                        case mnesia:add_table_copy(Table, Node, disc_copies) of
-                            {atomic, ok} ->
-                                io:format("Table ~p added to ~p~n", [Table, Node]);
-                            {aborted, Reason} ->
-                                io:format("Failed to add ~p to ~p: ~p~n", [Table, Node, Reason])
-                        end
-                    end, MissingNodes);
-                true ->
-                    io:format("Table ~p already on all nodes~n", [Table])
+        %% Сначала создаем таблицу как ram_copies на master
+        Opts = get_table_opts(Table, ram_copies),
+        case mnesia:create_table(Table, Opts) of
+            {atomic, ok} ->
+                io:format("Table ~p created on master as ram_copies~n", [Table]),
+                %% Преобразуем в disc_copies
+                case mnesia:change_table_copy_type(Table, node(), disc_copies) of
+                    {atomic, ok} ->
+                        io:format("Table ~p changed to disc_copies on master~n", [Table]);
+                    {aborted, Reason} ->
+                        io:format("Failed to change table ~p to disc_copies: ~p~n", [Table, Reason])
                 end;
-            false ->
-                %% Таблица не существует, создаем на всех узлах
-                io:format("Creating table ~p on all nodes~n", [Table]),
-                create_table_on_nodes(Table, AllNodes)
+            {aborted, {already_exists, _}} ->
+                io:format("Table ~p already exists~n", [Table]);
+            {aborted, Reason} ->
+                io:format("Failed to create table ~p: ~p~n", [Table, Reason])
         end
     end, Tables).
 
-%% @private Создание таблицы на узлах
-create_table_on_nodes(Table, Nodes) ->
-    Opts = case Table of
+%% @private Добавление копий таблиц на slave узлы
+add_table_copies_to_slaves() ->
+    Tables = ?TABLES_NAME_LIST,
+    
+    lists:foreach(fun(Table) ->
+        lists:foreach(fun(Node) ->
+            io:format("Adding table ~p copy to ~p~n", [Table, Node]),
+            case mnesia:add_table_copy(Table, Node, disc_copies) of
+                {atomic, ok} -> 
+                    io:format("Successfully added ~p to ~p~n", [Table, Node]);
+                {aborted, {already_exists, _, _}} ->
+                    io:format("Table ~p already exists on ~p~n", [Table, Node]);
+                {aborted, Reason} -> 
+                    io:format("Failed to add ~p to ~p: ~p~n", [Table, Node, Reason])
+            end
+        end, ?SLAVE_NODES)
+    end, Tables).
+
+%% @private Получить опции для создания таблицы
+get_table_opts(Table, StorageType) ->
+    case Table of
         users ->
-            [{type, set}, {disc_copies, Nodes}, {record_name, users},
+            [{type, set}, {StorageType, [node()]}, {record_name, users},
              {attributes, record_info(fields, users)}, {index, [domain]}];
         registrations ->
-            [{type, bag}, {disc_copies, Nodes}, {record_name, registrations},
+            [{type, bag}, {StorageType, [node()]}, {record_name, registrations},
              {attributes, record_info(fields, registrations)}];
         call ->
-            [{type, set}, {disc_copies, Nodes}, {record_name, call},
+            [{type, set}, {StorageType, [node()]}, {record_name, call},
              {attributes, record_info(fields, call)}];
         dialplan ->
-            [{type, set}, {disc_copies, Nodes}, {record_name, dialplan},
+            [{type, set}, {StorageType, [node()]}, {record_name, dialplan},
              {attributes, record_info(fields, dialplan)}]
+    end.
+
+%% @doc Обеспечить репликацию данных на все узлы (для slave)
+-spec ensure_replication() -> ok | {error, term()}.
+ensure_replication() ->
+    io:format("Ensuring Mnesia replication on ~p~n", [node()]),
+    
+    %% Запускаем Mnesia если не запущена
+    case mnesia:system_info(is_running) of
+        yes ->
+            io:format("Mnesia already running~n");
+        no ->
+            io:format("Starting Mnesia~n"),
+            mnesia:start(),
+            timer:sleep(1000)
     end,
-
-    case mnesia:create_table(Table, Opts) of
-        {atomic, ok} ->
-            io:format("Table ~p created~n", [Table]);
-        {aborted, {already_exists, _}} ->
-            ok;
-        {aborted, Reason} ->
-            io:format("Failed to create table ~p: ~p~n", [Table, Reason])
-    end.
-
-%% @private Получить список узлов с копией таблицы
-get_table_copies(Table) ->
-    case catch mnesia:table_info(Table, disc_copies) of
-        {'EXIT', _} -> [];
-        Nodes -> Nodes
-    end.
+    
+    %% Подключаемся к мастер узлу
+    case net_adm:ping(?MASTER_NODE) of
+        pong -> 
+            io:format("Connected to master node~n");
+        pang ->
+            io:format("Cannot connect to master node~n"),
+            {error, cannot_connect_to_master}
+    end,
+    
+    %% Ждем появления таблиц от мастера
+    wait_for_sync(30000),
+    
+    %% Проверяем статус
+    print_replication_status(),
+    
+    ok.
 
 %% @doc Ожидание синхронизации таблиц
 -spec wait_for_sync(Timeout :: non_neg_integer()) -> ok | {error, term()}.
 wait_for_sync(Timeout) ->
     Tables = ?TABLES_NAME_LIST,
+    
+    %% Ждем пока таблицы появятся
+    wait_for_tables_existence(Tables, Timeout),
+    
+    %% Проверяем существующие таблицы
     ExistingTables = [T || T <- Tables, mnesia:table_info(T, exists)],
-
+    
     case ExistingTables of
         [] ->
+            io:format("Warning: No tables found~n"),
             {error, no_tables};
         _ ->
             io:format("Waiting for tables: ~p~n", [ExistingTables]),
@@ -150,8 +184,27 @@ wait_for_sync(Timeout) ->
             end
     end.
 
-%% @doc Вывод статуса репликации
--spec print_replication_status() -> ok.
+%% @private Ожидание появления таблиц
+wait_for_tables_existence(_Tables, 0) ->
+    ok;
+wait_for_tables_existence(Tables, Timeout) ->
+    AllExist = lists:all(fun(Table) -> 
+        try
+            mnesia:table_info(Table, exists),
+            true
+        catch
+            _:_ -> false
+        end
+    end, Tables),
+    
+    if AllExist ->
+        ok;
+    true ->
+        timer:sleep(1000),
+        wait_for_tables_existence(Tables, Timeout - 1000)
+    end.
+
+%% @private Вывод статуса репликации
 print_replication_status() ->
     io:format("=== Mnesia Replication Status ===~n"),
     io:format("Current node: ~p~n", [node()]),
@@ -160,45 +213,12 @@ print_replication_status() ->
 
     Tables = ?TABLES_NAME_LIST,
     lists:foreach(fun(Table) ->
-        case catch mnesia:table_info(Table, disc_copies) of
-            {'EXIT', _} ->
-                io:format("Table ~p: missing~n", [Table]);
-            Nodes ->
-                io:format("Table ~p: copies on ~p~n", [Table, Nodes])
+        case mnesia:table_info(Table, exists) of
+            true ->
+                Nodes = mnesia:table_info(Table, disc_copies),
+                io:format("Table ~p: copies on ~p~n", [Table, Nodes]);
+            _ ->
+                io:format("Table ~p: missing~n", [Table])
         end
     end, Tables),
     io:format("================================~n").
-
-%% @doc Установка кластера с нуля
--spec setup_cluster() -> ok.
-setup_cluster() ->
-    io:format("Setting up Mnesia cluster~n"),
-
-    %% Останавливаем Mnesia если запущена
-    case mnesia:system_info(is_running) of
-        yes -> mnesia:stop();
-        no -> ok
-    end,
-
-    %% Удаляем старую схему на всех узлах
-    lists:foreach(fun(Node) ->
-        rpc:call(Node, mnesia, delete_schema, [[Node]]),
-        rpc:call(Node, mnesia, stop, [])
-    end, ?NODE_LIST),
-
-    %% Создаем схему на мастере
-    rpc:call(?MASTER_NODE, mnesia, create_schema, [[?MASTER_NODE]]),
-
-    %% Запускаем Mnesia на всех узлах
-    lists:foreach(fun(Node) ->
-        rpc:call(Node, mnesia, start, [])
-    end, ?NODE_LIST),
-
-    %% Добавляем slave узлы в схему (выполняем на мастере)
-    rpc:call(?MASTER_NODE, ?MODULE, add_nodes_to_schema, [?SLAVE_NODES]),
-
-    %% Создаем таблицы на мастере
-    rpc:call(?MASTER_NODE, ?MODULE, ensure_tables_replicated, [?NODE_LIST]),
-
-    io:format("Cluster setup complete~n"),
-    ok.
